49d19060951d34b39bb5b683694eaa5ce65da29c
shubonnsei <110585645+shubonnsei@users.noreply.github.com>
2026-04-23 23:50:35 +0900

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        for (int i = 0;i < nums.length;i++){
+            for (int j = 0;j<i;j++){
+                if (nums[i] == nums[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
